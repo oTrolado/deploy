242e3db3a4d2d88a2cfe63960d4b77a3e0680f50
oTrolado <gabriel.costa41@fatec.sp.gov.br>
2019-06-24 17:41:38 -0300

    --

--- controllers/notifications.controller.js
+++ controllers/notifications.controller.js
@@ -1,15 +1,43 @@
 const Notification = require('../models/Notification') ();
+const io = require('socket.io')(3001);
+var sock;
+var users = {}
+
+io.on('connection', socket => {
+    socket.emit('teste', 'Conectado');
+    
+    socket.on('login', (data) => {        
+        users[data] = socket;
+        socket.emit('notification', 'autenticado');
+    });
+    
+    socket.on('disconnect', data => {
+        if(!data) return;
+        delete users[data];
+    });
+});
+
 
 const notification = {
 
-    new: (message, users) => {
-        users.map( user => {
-            Notification.create({user: user, message: message, state: 'created', date: new Date()})
-            .then(
-                success => {},
-                error => console.error(error)
-            );
+    new: (message, usersM) => {
+
+        connectedUsers = Object.keys(users);
+        
+        usersM.forEach(userM => {
+            send = connectedUsers.some( conneted => conneted === userM.user);
+            console.log(send)
+            if(send)
+                users[userM.user].emit('notification', message);
         });
+
+        // usersM.map( user => {
+        //     Notification.create({user: user, message: message, state: 'created', date: new Date()})
+        //     .then(
+        //         success => {},
+        //         error => console.error(error)
+        //     );
+        // });
     },
 
     clearAll: () => {
